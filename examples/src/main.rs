636730ece3d2c844d320fea8733b20f3070dd70b
Sergey Isaev <isvforall@gmail.com>
2019-07-02 02:08:24 +0300

    update example

--- examples/src/main.rs
+++ examples/src/main.rs
@@ -1,15 +1,17 @@
-use cex_trading::api::CexAPI;
-use cex_trading::models::Symbol;
+use cexio::api::CexAPI;
+use cexio::models::Symbol;
 
-const CEX_USERID: &str = ""; // your user id
-const CEX_API_KEY: &str = ""; // your api key
-const CEX_API_SECRET: &str = ""; // your api secret
+const CEX_USER_ID: &str = "your user id";
+const CEX_API_KEY: &str = "your api key";
+const CEX_API_SECRET: &str = "your api secret";
 
 fn main() {
+
     // Public API calls
     println!("{:?}", CexAPI::last_price(Symbol::BTC, Symbol::USD));
 
     // Private API calls
-    let cex = CexAPI::new(CEX_USERID, CEX_API_KEY, CEX_API_SECRET);
+    let cex = CexAPI::new(CEX_USER_ID, CEX_API_KEY, CEX_API_SECRET);
     println!("{:?}", cex.balance());
+
 }
